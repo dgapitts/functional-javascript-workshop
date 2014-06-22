368ad65e3278fb24d45947c10a853be02e4c41db
Dave Pitts <dgapitts@gmail.com>
2014-06-22 08:39:39 +0200

    Update ex5_array_every.js
    Tidying up code.. format/alignment and comment changes only i.e. no functional changes

--- ex5_array_every.js
+++ ex5_array_every.js
@@ -1,14 +1,13 @@
 function checkUsersValid(goodUsers) {
-	  //console.log(goodUsers)
-	  var map = Array.prototype.map;
-      var goodUsersIDs = map.call(goodUsers, function(x) { return x.id; });
+	//console.log(goodUsers)
+	var map = Array.prototype.map;
+        var goodUsersIDs = map.call(goodUsers, function(x) { return x.id; });
 
-      //console.log(goodUsersIDs)	
-      return function(submittedUsers) {
+        //console.log(goodUsersIDs)	
+        return function(submittedUsers) {
       	var submittedUsersIDs = map.call(submittedUsers, function(x) { return x.id; });
       	var passed = submittedUsersIDs.every((function (x) { return goodUsersIDs.indexOf(x) > -1}))
       	return passed;
-        // SOLUTION GOES HERE
       };
     }
 
